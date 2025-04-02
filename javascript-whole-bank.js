5d9b4dc0105d0e53afc4ab478a3417f28ce3ff56
levidegeer <levidegeer@gmail.com>
2025-04-02 14:34:26 -0700

    a

---
+++ javascript-whole-bank.js
@@ -0,0 +1,10 @@
+var n;
+console.log(typeof n);
+var a = "3.4";
+console.log(typeof a);
+var textArray = ["Welcome back", "Incorrect answer", "Enter a name"];
+console.log(typeof textArray);
+var c = 2.4;
+console.log(typeof c);
+var tag = null;
+console.log(typeof tag);
